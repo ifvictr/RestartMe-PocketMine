3dd50d4b5189b9707ab636b938b303582d1e760a
Gamecrafter <gamecraftervt@gmail.com>
2016-05-30 17:36:35 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+![RestartMe](images/icon.png)
+#RestartMe
+Make sure your server stays up all the time!
+
+###Commands:
+Main command: **restartme**, **rm**
+
+|Sub-command|Description|
+|-----------|-----------|
+|**add**, **a**|Adds n seconds to the timer|
+|**help**|Shows all RestartMe commands|
+|**memory**, **m**|Shows memory usage information|
+|**set**|Sets the timer to n seconds|
+|**start**|Starts the timer|
+|**stop**|Stops the timer|
+|**subtract**, **s**|Subtracts n seconds from the timer|
+|**time**, **t**|Gets the remaining time until the server restarts|
+
+###Permissions:
+|Node|Default|
+|----|:-------:|
+|restartme.command.restartme|op|
+
+###Releases:
+|Version|Release Date|Download|
+|:-------:|------------|:--------:|
+|1.0.0|September 10, 2015|[available](https://github.com/Gamecrafter/PocketMine-Plugins/blob/master/RestartMe/releases/RestartMe_v1.0.0.phar?raw=true)|
+|1.1.0|October 12, 2015|[available](http://forums.pocketmine.net/plugins/restartme.1509/download?version=2836)|
+|1.2.0|December 25, 2015|[available](http://forums.pocketmine.net/plugins/restartme.1509/download?version=2907)|
+|1.3.0|?|unavailable|
---
+++ src/restartme/utils/Utils.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace restartme\utils;
+
+class Utils{
+    /**
+     * Calculates the memory usage threshold from a string
+     * @param string $toCheck
+     * @return int
+     */
+    public static function calculateBytes($toCheck){
+        $byteLimit = (int) substr(trim($toCheck), 0, 1);
+        switch(strtoupper(substr($toCheck, -1))){
+            case "P": //petabyte
+                return $byteLimit * pow(1024, 5);
+            case "T": //terabyte
+                return $byteLimit * pow(1024, 4);
+            case "G": //gigabyte
+                return $byteLimit * pow(1024, 3);
+            case "M": //megabyte
+                return $byteLimit * pow(1024, 2);
+            case "K": //kilobyte
+                return $byteLimit * 1024;
+            case "B": //byte
+                return $byteLimit;
+            default:
+                return $byteLimit;
+        }
+        return $byteLimit;
+    }
+    /**
+     * Returns true if $toCheck is greater than the current memory usage
+     * @param string $toCheck
+     * @return bool
+     */
+    public static function isOverloaded($toCheck){
+        return memory_get_usage(true) > self::calculateBytes($toCheck);
+    }
+    /**
+     * Returns 0 => hours, 1 => minutes, 2 => seconds, calculated from $time
+     * @param int $time
+     * @return array
+     * @throws \InvalidArgumentException
+     */
+    public static function toArray($time){
+        if(is_int($time)){
+            return [
+                floor($time / 3600), //hour
+                floor(($time / 60) - (floor($time / 3600) * 60)), //minute
+                floor($time % 60) //second
+            ];
+        }
+        else{
+            throw new \InvalidArgumentException("Expected integer, ".gettype($time)." given.");
+        }
+    }
+}
